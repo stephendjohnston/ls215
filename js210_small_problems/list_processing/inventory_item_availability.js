fa40f72f39cff5bae52e2138661a92af6a6f5761
Stephen Johnston <sdjohnston1990@gmail.com>
2021-04-22 10:40:59 -0300

    Add inventory item exercises

---
+++ js210_small_problems/list_processing/inventory_item_availability.js
@@ -0,0 +1,44 @@
+function transactionsFor(inventoryItem, transactions) {
+  return transactions.filter(({id}) => id === inventoryItem);
+}
+
+function itemQuantity(items) {
+  return items.reduce((sum, {movement, quantity}) => {
+    return sum + (movement === 'in' ? quantity : -quantity);
+  }, 0);
+}
+
+function isItemAvailable(inventoryItem, transactions) {
+  let items = transactionsFor(inventoryItem, transactions);
+  let quantity = itemQuantity(items);
+
+  return quantity > 0;
+}
+
+const transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                       { id: 105, movement: 'in',  quantity: 10 },
+                       { id: 102, movement: 'out', quantity: 17 },
+                       { id: 101, movement: 'in',  quantity: 12 },
+                       { id: 103, movement: 'out', quantity: 15 },
+                       { id: 102, movement: 'out', quantity: 15 },
+                       { id: 105, movement: 'in',  quantity: 25 },
+                       { id: 101, movement: 'out', quantity: 18 },
+                       { id: 102, movement: 'in',  quantity: 22 },
+                       { id: 103, movement: 'out', quantity: 15 }, ];
+
+isItemAvailable(101, transactions);     // false
+isItemAvailable(105, transactions);     // true
+
+// LS Solution:
+
+function isItemAvailable(item, transactions) {
+  const quantity = transactionsFor(item, transactions).reduce((sum, transaction) => {
+    if (transaction.movement === 'in') {
+      return sum + transaction.quantity;
+    } else {
+      return sum - transaction.quantity;
+    }
+  }, 0);
+
+  return quantity > 0;
+}
