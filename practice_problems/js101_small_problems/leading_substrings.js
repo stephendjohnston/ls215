70e8f42c953f1fd1de70c82a250ba2618668b410
Stephen Johnston <sdjohnston1990@gmail.com>
2021-06-10 07:56:22 -0300

    Add Leading Substrings

--- practice_problems/js101_small_problems/leading_substrings.js
+++ practice_problems/js101_small_problems/leading_substrings.js
@@ -33,6 +33,14 @@ function leadingSubstrings(string) {
   }, []);
 }
 
+// Using map
+
+function leadingSubstrings(string) {
+  return string.split('').map((_, idx) => {
+    return string.slice(0, idx + 1);
+  });
+}
+
 leadingSubstrings('abc');      // ["a", "ab", "abc"]
 leadingSubstrings('a');        // ["a"]
 leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
