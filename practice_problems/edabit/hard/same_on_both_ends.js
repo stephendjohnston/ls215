bb3ef292fe901126d652e5ba1eee4003efd03cf2
Stephen Johnston <sdjohnston1990@gmail.com>
2021-06-22 16:01:06 -0300

    Add Same On Both Ends

--- practice_problems/edabit/hard/same_on_both_ends.js
+++ practice_problems/edabit/hard/same_on_both_ends.js
@@ -5,15 +5,18 @@ function countSameEnds(sentence) {
     if (!/[a-z]/gi.test(word[word.length - 1])) {
       if (word[0] === word[word.length - 2]) {
         return word;
-      } else if (word[0] === word[word.length - 1]) {
-        return word;
       }
+    } else if (word.length > 1 && word[0] === word[word.length - 1]) {
+      return word;
     }
   });
 
   return result.length;
 }
 
-countSameEnds("Pop! goes the balloon")   // ➞ 1
-countSameEnds("And the crowd goes wild!") // ➞ 0
-countSameEnds("No I am not in a gang.")   // ➞ 1
+console.log(countSameEnds("Pop! goes the balloon"))    // ➞ 1
+console.log(countSameEnds("And the crowd goes wild!")) // ➞ 0
+console.log(countSameEnds("No I am not in a gang."))   // ➞ 1
+console.log(countSameEnds('My mom is not a nun.'));    //   2
+console.log(countSameEnds('A fine morning'));          //   0
+console.log(countSameEnds('Taste the difference'));    //   0
